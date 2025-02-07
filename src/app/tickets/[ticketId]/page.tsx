2e44eaf26ba265b525aad7cf27a2fdfeba09c7c7
temcha <itemchur@spin.ai>
2025-02-07 19:19:19 +0200

    added navigation links

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,11 +1,23 @@
+import { initialTickets } from "@/data";
+
 const TicketPage = async ({
   params,
 }: {
   params: Promise<{ ticketId: string }>;
 }) => {
   const { ticketId } = await params;
+  const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
 
-  return <div>My Ticket: {ticketId}</div>;
+  if (!ticket) {
+    return <div>Ticket not found</div>;
+  }
+
+  return (
+    <div>
+      <h2 className="text-lg">{ticket.title}</h2>
+      <p className="text-sm">{ticket.content}</p>
+    </div>
+  );
 };
 
 export default TicketPage;
