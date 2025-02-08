27fb26c0cd31d9f3634f5a3c534d999e748c19cc
temcha <itemchur@spin.ai>
2025-02-08 13:23:48 +0200

    added ticket item component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,21 +1,6 @@
-import { LucideCheckCircle, LucideFileText, LucidePencil } from "lucide-react";
-import Link from "next/link";
 import { Heading } from "@/components/heading";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { initialTickets } from "@/data";
-import { ticketPath } from "@/paths";
-
-const TICKET_ICONS = {
-  OPEN: <LucideFileText className="w-4 h-4" />,
-  DONE: <LucideCheckCircle className="w-4 h-4" />,
-  IN_PROGRESS: <LucidePencil className="w-4 h-4" />,
-};
+import TicketItem from "@/features/ticket/components/ticket-item";
 
 const TicketsPage = () => {
   return (
@@ -24,24 +9,7 @@ const TicketsPage = () => {
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
         {initialTickets.map((ticket) => (
-          <Card key={ticket.id} className="w-full max-w-[420px]">
-            <CardHeader>
-              <CardTitle className="flex align-middle gap-x-2">
-                <span>{TICKET_ICONS[ticket.status]}</span>
-                <h3 className="truncate">{ticket.title}</h3>
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className="line-clamp-3 whitespace-break-spaces">
-                {ticket.content}
-              </p>
-            </CardContent>
-            <CardFooter>
-              <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                View
-              </Link>
-            </CardFooter>
-          </Card>
+          <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
     </div>
---
+++ src/features/ticket/constants.tsx
@@ -0,0 +1,7 @@
+import { LucideCheckCircle, LucideFileText, LucidePencil } from "lucide-react";
+
+export const TICKET_ICONS = {
+  OPEN: <LucideFileText className="w-4 h-4" />,
+  DONE: <LucideCheckCircle className="w-4 h-4" />,
+  IN_PROGRESS: <LucidePencil className="w-4 h-4" />,
+};
