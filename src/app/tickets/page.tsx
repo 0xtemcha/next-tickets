f7126ceecdb53ccbc5fcf6ab318b0309518668a4
temcha <itemchur@spin.ai>
2025-02-09 16:00:11 +0200

    added force-dynamic example

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,6 +3,9 @@ import { Heading } from "@/components/heading";
 import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 
+// Exmaple of dynamic route rendering
+// export const dynamic = "force-dynamic";
+
 const TicketsPage = async () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
