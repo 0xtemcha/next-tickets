71a7864e8a4c13727889bfb11c278988149e622f
temcha <itemchur@spin.ai>
2025-02-14 16:35:20 +0200

    added root template with redirect toast component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import { CardCompact } from "@/components/card-compact";
 import { Heading } from "@/components/heading";
-import { RedirectToast } from "@/components/redirect-toast";
 import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
@@ -12,23 +11,20 @@ import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-for
 
 const TicketsPage = async () => {
   return (
-    <>
-      <div className="flex-1 flex flex-col gap-y-8">
-        <Heading title="Tickets" description="All your tickets at one place" />
+    <div className="flex-1 flex flex-col gap-y-8">
+      <Heading title="Tickets" description="All your tickets at one place" />
 
-        <CardCompact
-          title="Create Ticket"
-          description="A new ticket will be created"
-          className="w-full max-w-[420px] self-center"
-          content={<TicketUpsertForm />}
-        />
+      <CardCompact
+        title="Create Ticket"
+        description="A new ticket will be created"
+        className="w-full max-w-[420px] self-center"
+        content={<TicketUpsertForm />}
+      />
 
-        <Suspense fallback={<Spinner />}>
-          <TicketList />
-        </Suspense>
-      </div>
-      <RedirectToast />
-    </>
+      <Suspense fallback={<Spinner />}>
+        <TicketList />
+      </Suspense>
+    </div>
   );
 };
 
