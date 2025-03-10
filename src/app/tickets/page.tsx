0cded7a1f03b576b5117e19343fba28ca9edd68b
temcha <itemchur@spin.ai>
2025-03-10 22:55:54 +0200

    #ui-shadcn-2: added shadcn card, replaced basic ticket card with shadcn card

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -4,8 +4,14 @@ import {
 	LucideStickyNote,
 } from 'lucide-react';
 import Link from 'next/link';
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
 import { initialTickets } from '@/data';
-import { cn } from '@/lib/utils';
 import { ticketPath } from '@/paths';
 
 const TICKET_ICONS = {
@@ -26,23 +32,24 @@ const TicketsPage = () => {
 
 			<div className="animate-fade-from-top flex flex-1 flex-col items-center gap-y-4">
 				{initialTickets.map((ticket) => (
-					<div
-						key={ticket.id}
-						className="w-full max-w-[420px] rounded border p-4"
-					>
-						<div>{TICKET_ICONS[ticket.status]}</div>
-						<h3 className="truncate text-lg font-semibold">{ticket.title}</h3>
-						<p
-							className={cn('text-muted-foreground truncate text-sm', {
-								'line-through': ticket.status === 'DONE',
-							})}
-						>
-							{ticket.content}
-						</p>
-						<Link href={ticketPath(ticket.id)} className="text-sm underline">
-							View
-						</Link>
-					</div>
+					<Card key={ticket.id} className="w-full max-w-[420px]">
+						<CardHeader>
+							<CardTitle className="flex items-center gap-x-2">
+								<span>{TICKET_ICONS[ticket.status]}</span>
+								<h3 className="truncate">{ticket.title}</h3>
+							</CardTitle>
+						</CardHeader>
+						<CardContent>
+							<span className="line-clamp-3 whitespace-break-spaces">
+								{ticket.content}
+							</span>
+						</CardContent>
+						<CardFooter>
+							<Link href={ticketPath(ticket.id)} className="text-sm underline">
+								View
+							</Link>
+						</CardFooter>
+					</Card>
 				))}
 			</div>
 		</div>
