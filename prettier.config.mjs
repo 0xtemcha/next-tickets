2c0f31f852afc21118729569e937d1886090d160
temcha <itemchur@spin.ai>
2025-03-10 16:21:38 +0200

    added README file to prettier mdx overrides

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,46 +1,46 @@
 const config = {
-  arrowParens: "always",
-  bracketSameLine: false,
-  bracketSpacing: true,
-  embeddedLanguageFormatting: "auto",
-  endOfLine: "lf",
-  htmlWhitespaceSensitivity: "css",
-  insertPragma: false,
-  jsxSingleQuote: false,
-  printWidth: 80,
-  proseWrap: "always",
-  quoteProps: "as-needed",
-  requirePragma: false,
-  semi: true,
-  singleAttributePerLine: false,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: "all",
-  useTabs: true,
-  overrides: [
-    // formatting the package.json with anything other than spaces will cause
-    // issues when running install...
-    {
-      files: ["**/package.json"],
-      options: {
-        useTabs: false,
-      },
-    },
-    {
-      files: ["**/*.mdx"],
-      options: {
-        // This stinks, if you don't do this, then an inline component on the
-        // end of the line will end up wrapping, then the next save Prettier
-        // will add an extra line break. Super annoying and probably a bug in
-        // Prettier, but until it's fixed, this is the best we can do.
-        proseWrap: "preserve",
-        htmlWhitespaceSensitivity: "ignore",
-      },
-    },
-  ],
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindAttributes: ["class", "className", "ngClass", ".*[cC]lassName"],
-  tailwindFunctions: ["clsx", "cn"],
+	arrowParens: 'always',
+	bracketSameLine: false,
+	bracketSpacing: true,
+	embeddedLanguageFormatting: 'auto',
+	endOfLine: 'lf',
+	htmlWhitespaceSensitivity: 'css',
+	insertPragma: false,
+	jsxSingleQuote: false,
+	printWidth: 80,
+	proseWrap: 'always',
+	quoteProps: 'as-needed',
+	requirePragma: false,
+	semi: true,
+	singleAttributePerLine: false,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'all',
+	useTabs: true,
+	overrides: [
+		// formatting the package.json with anything other than spaces will cause
+		// issues when running install...
+		{
+			files: ['**/package.json'],
+			options: {
+				useTabs: false,
+			},
+		},
+		{
+			files: ['**/*.mdx', 'README.md'],
+			options: {
+				// This stinks, if you don't do this, then an inline component on the
+				// end of the line will end up wrapping, then the next save Prettier
+				// will add an extra line break. Super annoying and probably a bug in
+				// Prettier, but until it's fixed, this is the best we can do.
+				proseWrap: 'preserve',
+				htmlWhitespaceSensitivity: 'ignore',
+			},
+		},
+	],
+	plugins: ['prettier-plugin-tailwindcss'],
+	tailwindAttributes: ['class', 'className', 'ngClass', '.*[cC]lassName'],
+	tailwindFunctions: ['clsx', 'cn'],
 };
 
 export default config;
